0619581593a8c5e8dafa924c9935587509507cde
Albena Kr <albena.krasteva@gmail.com>
2020-02-29 23:29:27 +0000

    Add item functionality added

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo_list.html')
+    results = Item.objects.all()
+    return render(request, 'todo_list.html', {'Items': results})
+
+def create_an_item(request):
+    if request.method=="POST":
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
+    else:
+        form = ItemForm()
+
+    return render(request, 'item_form.html', {'form': form})
