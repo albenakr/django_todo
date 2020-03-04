7cfe19c47e27d6f901e920f45ef2a8df34d5a010
Albena Kr <albena.krasteva@gmail.com>
2020-03-04 21:14:19 +0000

    Spaces

--- todo/views.py
+++ todo/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
+
 # Create your views here.
 def get_todo_list(request):
     results = Item.objects.all()
     return render(request, 'todo_list.html', {'Items': results})
 
+
 def create_an_item(request):
-    if request.method=="POST":
+    if request.method == "POST":
         form = ItemForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
@@ -17,6 +19,7 @@ def create_an_item(request):
         form = ItemForm()
 
     return render(request, 'item_form.html', {'form': form})
+
 
 def edit_an_item(request, id):
     item = get_object_or_404(Item, pk=id)
